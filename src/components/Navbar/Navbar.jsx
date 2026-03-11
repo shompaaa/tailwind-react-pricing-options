9b39ae741e83629f6fc4f665147b75ac88914f76
shompaaa <shompaakter334@gmail.com>
2026-03-11 12:43:58 +0600

    menu button toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,60 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const navigationsData = [
   {
-    "id": 1,
-    "name": "Home",
-    "path": "/"
+    id: 1,
+    name: "Home",
+    path: "/",
   },
   {
-    "id": 2,
-    "name": "About",
-    "path": "/about"
+    id: 2,
+    name: "About",
+    path: "/about",
   },
   {
-    "id": 3,
-    "name": "Services",
-    "path": "/services"
+    id: 3,
+    name: "Services",
+    path: "/services",
   },
   {
-    "id": 4,
-    "name": "Blog",
-    "path": "/blog"
+    id: 4,
+    name: "Blog",
+    path: "/blog",
   },
   {
-    "id": 5,
-    "name": "Contact",
-    "path": "/contact"
-  }
-]
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
 
 const Navbar = () => {
-    return (
-        <div>
+  const [open, setOpen] = useState(false);
 
-        {/* Dynamic Navbar by passing data as props to another file */}
-            <nav>
-                <ul className='flex'>
-                    {
-                        navigationsData.map(route => <Link route={route}></Link>)
-                    }
-                </ul>
-            </nav>
+  const links = navigationsData.map((route) => <Link route={route}></Link>);
 
+  return (
+    <div>
+      {/* Dynamic Navbar by passing data as props to another file */}
+      <nav className="flex justify-between mx-10">
+        <span className="flex" onClick={() => setOpen(!open)}>
+          {open ? (
+            <X className="md:hidden"></X>
+          ) : (
+            <Menu className="md:hidden"></Menu>
+          )}
+          <ul className="md:hidden">
+            {links}
+          </ul>
+          <h2 className="ml-4">Navbar</h2>
+        </span>
+        <ul className="md:flex hidden">{links}</ul>
+        <button>Sign Up</button>
+      </nav>
 
-        {/* //Dynamic Navbar
+      {/* //Dynamic Navbar
         <nav>
             <ul className='flex'>
                 {
@@ -52,19 +63,17 @@ const Navbar = () => {
             </ul>
         </nav> */}
 
+      {/* Manual Navbar */}
 
-
-            {/* Manual Navbar */}
-
-            {/* <ul className='flex'>
+      {/* <ul className='flex'>
                 <li className='mr-10'><a href="/">Blog</a></li>
                 <li className='mr-10'><a href="/">Home</a></li>
                 <li className='mr-10'><a href="/">About Us</a></li>
                 <li className='mr-10'><a href="/">Services</a></li>
                 <li className='mr-10'><a href="/">Contact</a></li>
             </ul> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
